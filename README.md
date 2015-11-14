5fb99eb631ec8929c9995ee87b38bd15a9a5d654
Fredrik Gustafsson <gusten@dindinoh.net>
2015-11-14 19:08:30 +0000

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# mailgunme
+binary to fastly be fetched from bintray and used from cli to send emails using port 80
+
+# ToDo
+accept message as piped?
+
---
+++ mailgunme.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+// Config is the struct for config file in ~/.mailgunme
+type Config struct {
+	Hardcoded struct {
+		PublicKey, PrivateKey, Domain string
+	}
+}
+
+// parse_config reads config file in ~/.mailgunme
+func parse_config() Config {
+	var cfg Config
+	err := gcfg.ReadFileInto(&cfg, "~/.mailgunme")
+
+	if err != nil {
+		log.Fatalf("Failed to parse mailgunme config file in ~/.mailgunme", err)
+	}
+	return
+}
+
+// send will do the actual call
+func send(cfg Config, to, msg string) {
+	fmt.Println("CURL")
+}
+
+// main parses config and cli options and calls send function
+func main() {
+	parse_config()
+
+	toPtr := flag.String("to", "", "recipient address")
+	msgPtr := flag.String("msg", "", "message")
+
+	gun := mailgun.NewMailgun("valid-mailgun-domain", "private-mailgun-key", "public-mailgun-key")
+
+	m := mailgun.NewMessage("Sender <sender@example.com>", "Subject", "Message Body", "Recipient <recipient@example.com>")
+	response, id, _ := gun.Send(m)
+	fmt.Printf("Response ID: %s\n", id)
+	fmt.Printf("Message from server: %s\n", response)
+}
