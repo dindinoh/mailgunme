8960679b5e4ba02e02b515b42cba5cafbed0363e
Fredrik Gustafsson <gusten@dindinoh.net>
2016-11-03 10:47:14 +0100

    added go gets to buildscript

--- build_osx.sh
+++ build_osx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+go get github.com/mailgun/mailgun-go github.com/mitchellh/go-homedir gopkg.in/gcfg.v1
+
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build --ldflags '-extldflags "-static"' mailgunme.go
