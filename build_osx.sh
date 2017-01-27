bcaa8b46fe68d0fcc419d39576458218b9cabb36
Fredrik Gustafsson <gusten@dindinoh.net>
2017-01-28 00:00:42 +0100

    added mgm output to osx build script

--- build_osx.sh
+++ build_osx.sh
@@ -3,3 +3,5 @@
 go get github.com/mailgun/mailgun-go github.com/mitchellh/go-homedir gopkg.in/gcfg.v1
 
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build --ldflags '-extldflags "-static"' mailgunme.go
+
+mv mailgunme mgm.osx
